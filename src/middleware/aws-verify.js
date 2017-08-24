1abb51947d5075f1b82ac6ac4c02198d3d14e7a6
CU7IOUS <garkusha.sergiy@gmail.com>
2017-08-24 10:14:31 -0700

    Add AWS verify middleware

---
+++ src/middleware/aws-verify.js
@@ -0,0 +1,21 @@
+const verifier = require('alexa-verifier')
+
+module.exports = (req, res, next) => {
+  if (process.env.NODE_ENV === 'development') {
+    return next()
+  }
+
+  if (!req.body.session || !req.body.request || !req.headers.signaturecertchainurl) {
+    console.error(req.body, req.headers.signaturecertchainurl)
+    return res.status(400).json({ message: 'Invalid Request Body', error: null })
+  }
+
+  verifier(req.headers.signaturecertchainurl, req.headers.signature, req.rawBody, function (err) {
+    if (err) {
+      console.error(err)
+      res.status(401).json({ message: 'Verification Failure', error: err })
+    } else {
+      next()
+    }
+  })
+}
